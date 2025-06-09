0bae7d83de0ba15346718852eb4307e23ec98ad6
qtrix <bogdan.lacatusu13@gmail.com>
2025-06-09 15:42:25 +0300

    Updates

---
+++ assets/all-wallets-BCBPl4ph.js
@@ -0,0 +1,6 @@
+import{m as a}from"./core-B_wQ_eH3.js";import"./index-Dbu-SxGn.js";import"./index.es-C0vMqq8B.js";const i=a`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{i as allWalletsSvg};
---
+++ assets/cursor-BZh8J4NY.js
@@ -0,0 +1,3 @@
+import{m as o}from"./core-B_wQ_eH3.js";import"./index-Dbu-SxGn.js";import"./index.es-C0vMqq8B.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};

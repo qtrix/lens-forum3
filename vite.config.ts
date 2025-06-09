3c36b52ae350be8e8d8c890f96690dc45648455b
qtrix <bogdan.lacatusu13@gmail.com>
2025-06-09 18:06:02 +0300

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lens-forum3/',
+  base: '../lens-forum-test/',
   plugins: [react()],
 })

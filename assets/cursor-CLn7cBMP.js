255123d7df4c3082d8eea7631275e42f41fd8fd0
qtrix <bogdan.lacatusu13@gmail.com>
2025-06-09 18:19:50 +0300

    Updates

---
+++ assets/cursor-CLn7cBMP.js
@@ -0,0 +1,3 @@
+import{m as o}from"./core-Qixtvu8U.js";import"./index-Capv1seO.js";import"./index.es-UaMRx_PI.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
